d4cbdd22df07dcb6adf114ef6ffb7010ed2ff138
Ashley Burns <ashley@ashleys-mini.home>
2021-02-07 13:55:38 -0500

    task 6 done

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ function removeArtist(array, index){
    array.splice(index,1);
    return array.length
 }
-console.log(removeArtist(artists, 0))  
+// console.log(removeArtist(artists, 0))  
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -287,10 +287,20 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+  let newArtist={ 
+    id: 20,
+    name: "Ashley Burns", 
+    years: 1993 - 2020,
+    genre: "Web Design", 
+    nationality: "Italian",
+    bio: "Ashley Marie Burns(31 March 1993- 7 Febuary 2020) a mother of two painter and web designer. Loved learning new things and ideas"
   }
+  array.push(newArtist);
+  return array;
+}
 
+// console.log(addArtist(artists))
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
